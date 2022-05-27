e66c170e659d4b4a20eb3d3b67bb161d0618d562
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-27 22:58:46 +0600

    project setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00ffff",
+          secondary: "#00ff80",
+          accent: "#e87345",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
