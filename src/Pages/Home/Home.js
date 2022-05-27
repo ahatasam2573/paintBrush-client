631706387bf009fea226e5d3c3a875d9bbd8091b
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-05-28 05:12:47 +0600

    blogs section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 import AvailableItems from './AvailableItems';
 import Banner from './Banner/Banner';
 import BusinessSummary from './BusinessSummary';
-import ImproveSection from './ImproveSection/ImproveSection';
-import ProductManagement from './ProductManagement/ProductManagement';
+import ImproveSection from './ImproveSection';
+import ProductManagement from './ProductManagement';
 import Testimonial from './Testimonial';
 
 const Home = () => {
